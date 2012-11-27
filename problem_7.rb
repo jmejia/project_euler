c29bc1c6fd52c927c44854a1750725a8eb40d60f
Josh <joshua.mejia@gmail.com>
2012-11-27 08:08:29 -0600

    added the problem as a comment to the top of each solution

--- problem_7.rb
+++ problem_7.rb
@@ -1,3 +1,6 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10 001st prime number?
+
 current_number = 2 # 1 is not prime but 2 is. Start at 2.
 prime_count = 1 # 2 is prime. Set the count at 1
 
